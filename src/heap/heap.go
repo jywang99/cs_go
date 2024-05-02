f6a30c7b7773a7a50f60c496c2c38d85afea5385
= <=>
2024-05-02 14:43:12 -0600

    heap

--- src/heap/heap.go
+++ src/heap/heap.go
@@ -1,14 +1,109 @@
 package heap
 
-type MaxHeap struct {
-    arr []int
+import (
+	"errors"
+)
+
+type MinHeap struct {
+    arr []*int
+    size int
+    cap int
 }
 
-func (heap *MaxHeap) Heapify() {
-
+func NewMinHeap(cap int) *MinHeap {
+    return &MinHeap{
+        arr: make([]*int, cap),
+        size: 0,
+        cap: cap,
+    }
 }
 
-func (heap *MaxHeap) ExtractMax() {
+func (h *MinHeap) parentIdx(i int) (int, bool) {
+    if i <= 0 {
+        return 0, false
+    }
+    return i/2, true
+}
 
+func (h *MinHeap) leftIdx(i int) (int, bool) {
+    li := i*2 + 1
+    return li, li < h.size
+}
+
+func (h *MinHeap) rightIdx(i int) (int, bool) {
+    ri := i*2 + 2
+    return ri, ri < h.size
+}
+
+func (h *MinHeap) swap(i, j int) {
+    tmp := h.arr[i]
+    h.arr[i] = h.arr[j]
+    h.arr[j] = tmp
+}
+
+func (h *MinHeap) heapify() {
+    h.heapifyRecurse(0)
+}
+
+func (h *MinHeap) heapifyRecurse(i int) {
+    li, lvalid := h.leftIdx(i)
+    ri, rvalid := h.rightIdx(i)
+
+    mi := i
+    if lvalid && *h.arr[li] < *h.arr[mi] {
+        mi = li
+    }
+    if rvalid && *h.arr[ri] < *h.arr[mi] {
+        mi = ri
+    }
+
+    if mi != i {
+        h.swap(mi, i)
+        h.heapifyRecurse(mi)
+    }
+}
+
+func (h *MinHeap) GetMin() *int {
+    return h.arr[0]
+}
+
+func (h *MinHeap) ExtractMin() *int {
+    if h.size <= 0 {
+        return nil
+    }
+    r := h.arr[0]
+
+    if h.size == 1 {
+        h.arr[0] = nil
+        h.size --
+        return r
+    }
+
+    h.arr[0] = h.arr[h.size-1]
+    h.size --
+    h.heapify()
+    return r
+}
+
+func (h *MinHeap) Insert(d int) error {
+    if h.size >= h.cap {
+        return errors.New("Heap is already full!")
+    }
+    dd := d
+    h.arr[h.size] = &dd
+    h.size ++
+    h.swim(h.size-1)
+    return nil
+}
+
+func (h *MinHeap) swim(i int) {
+    for ; i != 0; {
+        pi, pvalid := h.parentIdx(i)
+        if pvalid && *h.arr[i] >= *h.arr[pi] {
+            break
+        }
+        h.swap(i, pi)
+        i = pi
+    }
 }
 
--- src/heap/heap_test.go
+++ src/heap/heap_test.go
@@ -1,1 +1,31 @@
 package heap_test
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	hp "jy.org/csgo/src/heap"
+)
+
+func TestHeap(t *testing.T) {
+    h := hp.NewMinHeap(5)
+    h.Insert(10)
+    h.Insert(7)
+    h.Insert(8)
+    h.Insert(4)
+    h.Insert(1)
+
+    err := h.Insert(100)
+    assert.NotNil(t, err)
+
+    assert.Equal(t, 1, *h.GetMin())
+    assert.Equal(t, 1, *h.ExtractMin())
+    assert.Equal(t, 4, *h.GetMin())
+    assert.Equal(t, 4, *h.ExtractMin())
+    assert.Equal(t, 7, *h.ExtractMin())
+    assert.Equal(t, 8, *h.ExtractMin())
+    assert.Equal(t, 10, *h.ExtractMin())
+
+    var iptr *int = nil
+    assert.Equal(t, iptr, h.ExtractMin())
+}
